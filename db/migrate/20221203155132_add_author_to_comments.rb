b7f0457be2c1ea3b2af5bf498909195052d72224
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-12-04 03:49:56 +0430

    fix offence

--- db/migrate/20221203155132_add_author_to_comments.rb
+++ db/migrate/20221203155132_add_author_to_comments.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddAuthorToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :author, foreign_key: { to_table: :users}
+    add_reference :comments, :author, foreign_key: { to_table: :users }
   end
 end
