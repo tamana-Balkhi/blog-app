84982e46f8ad06669ec2f6bbfacc653be00652d4
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-12-03 16:43:29 +0430

    add refrences

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#git initblog'
+  root 'pages#blog'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
