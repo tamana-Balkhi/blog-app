a7fe6aec9cd7a2b961667a84bb9d6ac55b6ae043
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-12-04 08:59:37 +0430

    add test for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
+  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
-  def check_last_three_posts
-    posts.limit(3).order(created_at: :desc)
+  validates :name, presence: true
+  validates :postCounter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+
+  def last_three_posts
+    posts.order('created_at Desc').limit(3)
   end
 end
