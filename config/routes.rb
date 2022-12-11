317336561011f9cedaaf9f48a6e4aa63521b4643
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-12-11 06:35:35 +0430

    fix offence

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-      resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:new, :create]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[new create]
     end
   end
 end
