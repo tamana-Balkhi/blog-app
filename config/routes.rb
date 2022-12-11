d5ab41178e5d7a12ab7d51781e523443e69a1f2d
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-12-11 05:56:13 +0430

    add likes and comment indexes for rout

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-    # Defines the root path route ("/")
-    # root "articles#index"
+      resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
+    end
   end
 end
